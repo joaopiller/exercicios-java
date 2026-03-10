a0411871b19d3909c4fff5fddda0cb440e6dc428
João Vicente Piller <joaovicentepm14@gmail.com>
2026-03-10 15:00:17 -0300

    exercicio1

---
+++ src/exercicio1/Main.java
@@ -0,0 +1,35 @@
+import exercicio1.entities.Pessoa;
+
+void main() {
+    Locale.setDefault(Locale.US);
+    Scanner sc = new Scanner(System.in);
+    int n = 3;
+
+    List<Pessoa> pessoas = new ArrayList<>();
+
+    IO.println("Calculadora de IMC\n");
+    for (int i = 0; i < n; i++) {
+        IO.println("Pessoa " + (i + 1));
+        IO.print("Nome: ");
+        String nome = sc.nextLine();
+        IO.print("Altura: ");
+        double altura = sc.nextDouble();
+        IO.print("Peso: ");
+        double peso = sc.nextDouble();
+        sc.nextLine();
+        IO.println("");
+        pessoas.add(new Pessoa(nome, altura, peso));
+    }
+
+    double maiorImc = 0;
+    String nome = "";
+    for (Pessoa pessoa : pessoas) {
+        IO.println(pessoa);
+        if (pessoa.imc() > maiorImc) {
+            maiorImc = pessoa.imc();
+            nome = pessoa.getNome();
+        }
+    }
+
+    IO.println("\nPessoa com maior IMC: " + nome);
+}
---
+++ src/exercicio1/entities/Pessoa.java
@@ -0,0 +1,39 @@
+package exercicio1.entities;
+
+public class Pessoa {
+    private String nome;
+    private Double altura;
+    private Double peso;
+
+    public Pessoa(String nome, Double altura, Double peso) {
+        this.nome = nome;
+        this.altura = altura;
+        this.peso = peso;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Double imc() {
+        return peso / Math.pow(altura, 2);
+    }
+
+    public String classificacao() {
+        Double imc = imc();
+        if (imc < 18.5) {
+            return "Abaixo do peso";
+        } else if (imc < 25) {
+            return "Peso normal";
+        } else if (imc < 30) {
+            return "Sobrepeso";
+        } else {
+            return "Obesidade";
+        }
+    }
+
+    @Override
+    public String toString() {
+        return nome + " | IMC: " + String.format("%.2f", imc()) + " | " + classificacao();
+    }
+}
